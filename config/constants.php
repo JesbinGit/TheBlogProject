df5d14c7e443ff6f53d03a6b0c13d64b55758e96
Joel Joseph <joeljosephpam@gmail.com>
2023-11-02 16:59:14 +0530

    Initialized the following cofig/constants.php config/databse.php partials/header.php

---
+++ config/constants.php
@@ -0,0 +1,3 @@
+<?php
+    define('ROOT_URL','https://localhost/phpfile/project/TheBlogProject/'); //differend url for differend devices
+?>
--- index.php
+++ index.php
@@ -1,47 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Blog Website</title>
-   
-    <link rel = "stylesheet" href="./css/style.css">
-    <!--Icon Scout cdn for icons like the three lines or unicons-->
-    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.8/css/line.css">
-    <!--this is from google fonts-->
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;0,500;0,600;1,800&display=swap" rel="stylesheet">
-</head>
-<body>
-    <nav>
-        <div class = "container nav_container">
-            <a href="index.php" class="nav_logo"> The Blog Project </a>
-            <ul class ="nav_items">
-                <li><a href = "blog.php"> Blog  </a> </li>
-                <li><a href = "ourstory.php"> Our Story </a> </li>
-                <li><a href = "contact.php"> Contact Us </a> </li>
-                <li><a href = "signin.php"> Sign in  </a> </li>
-                
-                <li class ="nav_profile"> 
-                    <div class ="avatar">
-                        <img src="/images/jisoo.jpg">
-                    </div>
-                    <ul>
-                        <li> <a = href="dashboard.php"> Dashboard </a> </li>
-                        <li> <a = href="logout.php"> Logout </a> </li>
 
-                    </ul>
-
-
-                </li>
-            </ul>
-
-
-            <button id = "open_nav-btn"><i class="uil uil-bars"></i> </button>
-            <button id = "close_nav-btn"> <i class="uil uil-times-circle"></i></button>
-        </div>
-
-    </nav>
-    <!-- ================ END OF NAVIGATION BAR ================-->
  
 
     <section class=" featured">
