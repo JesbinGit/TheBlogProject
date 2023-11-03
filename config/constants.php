8f5c549959009d17aec045c8a535bcd526216f6b
JesbinGit <jesbinytofficial@gmail.com>
2023-11-03 22:12:23 +0530

    connnected database , added connection to config

--- config/constants.php
+++ config/constants.php
@@ -1,3 +1,11 @@
 <?php
-    define('ROOT_URL','https://localhost/phpfile/project/TheBlogProject/'); //differend url for differend devices
+   //define('ROOT_URL','https://localhost/phpfile/project/TheBlogProject/'); 
+    define('ROOT_URL','http://localhost/blog/TheBlogProject/'); 
+    //second one for the zephyrus
+    define('DB_HOST','localhost');
+    define('DB_USER','theblog');
+    define('DB_PASS','admin123');
+    define('DB_NAME','db_blog');
+    
+        
 ?>
