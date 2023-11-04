8c5f04399fdc77a3c958e9cf8efa213da7dd24bd
JesbinGit <jesbinytofficial@gmail.com>
2023-11-04 14:40:58 +0530

    almost complete code

--- signup-logic.php
+++ signup-logic.php
@@ -31,22 +31,55 @@ if (isset($_POST['submit'])) {
         // Hash the password
         $hashed_password = password_hash($createpassword, PASSWORD_DEFAULT);
          //couldnt find some small error for an hour so just rewrote the code upto here.
+         //same sanam thane but just some difference in else cycle.
 
          //check whether the username or email already exit in database
 
-         $user_check_query = "SELECT * FROM users WHERE userename = '$username' OR email ='$email'";
+         $user_check_query = "SELECT * FROM tb_users WHERE username = '$username' OR email ='$email'";
          $user_check_result = mysqli_query($connection, $user_check_query);
          if(mysqli_num_rows($user_check_result) > 0) {
             $_SESSION['signup'] = "User already exist";
          }
          else{
-            //work on avatar
-            
+            //avatar stuff
+            // rename avatar names cause same name files may exist
+            $time = time(); //using time as unique key for pics to make image names random
+            $avatar_name = $time . $avatar['name'];
+            $avatar_tmp_name = $avatar['tmp_name'];
+            $avatar_destination_path = 'images/' . $avatar_name;
+
+            //validating avatar
+            $allowed_files = ['png','jpg','jpeg'];
+            $extenstion = explode('.', $avatar_name);
+            $extenstion = end($extenstion);
+            if (in_array($extenstion, $allowed_files)){
+                // if yes check image is  not too large (1mb+)
+                if($avatar['size'] < 10000000){
+                    move_uploaded_file($avatar_tmp_name, $avatar_destination_path);
+                } else {
+                    $_SESSION['signup'] = "Image is too large , should be less than 1 mb or  Image file should be jpg , png or jpeg";
+                }    
+            }
          }
+        }   
+        //redirect back to signup page incase of signup
 
-    
+        if($_SESSION['signup']) {
+            header('location : ' . ROOT_URL. 'signup.php');
+            die();
+        }
+        else{
+            //insert user into table
+            $insert_user_query = "INSERT INTO tb_users (firstname , lastname , username , email , password , avatar , is_admin) VALUSE ('$firstname','$lastname', '$username', '$email', '$hashedpassword', '$avatar_name', 0 )";
+            
+            
+            if(!mysqli_errno($connection)){
+                $_SESSION['signup-sucess'] = "Registration successful. Please log in.";
+                header('location :' . ROOT_URL . 'sigin.php');
+                die();
+            }
+        }
 
-    
 } else {
     // If the signup button is not clicked, redirect to the signup page
     header('location: ' . ROOT_URL . 'signup.php');
@@ -54,3 +87,4 @@ if (isset($_POST['submit'])) {
 
    
 }
+
