44a4e4ff19f4a3e91f545ebc8757c3d84810adeb
JesbinGit <jesbinytofficial@gmail.com>
2023-11-05 17:43:26 +0530

    config revereted

--- config/constants.php
+++ config/constants.php
@@ -1,6 +1,8 @@
 <?php
 session_start();
-    require 'sys.php';  //alternative to swapping it every other second
+   // require 'sys.php';  //alternative to swapping it every other second
+    define('ROOT_URL','http://localhost/blog/TheBlogProject/'); 
+
     //second one for the zephyrus
     define('DB_HOST','localhost');
     define('DB_USER','theblog');
