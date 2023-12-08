8525d4fc24875eab0426b90ec137906b1f254a5f
Joel Joseph <joeljosephpam@gmail.com>
2023-12-08 18:52:32 +0530

    modified  edit-user.php so that it laods the user info Dynamically

--- admin/edit-user.php
+++ admin/edit-user.php
@@ -1,28 +1,38 @@
 <?php 
     require 'partials/header.php';
+
+  if(isset($_GET['id'])){
+    $id = filter_var($_GET['id'],FILTER_SANITIZE_NUMBER_INT);
+    $query = "SELECT * FROM tb_users WHERE id='$id'";
+    $result = mysqli_query($connection, $query);
+    $user = mysqli_fetch_assoc($result);
+  } else {
+    header('location: '. ROOT_URL . '/admin/manage-users.php');
+  }
+
 ?>
 
 
 <section class="form_section">
     <div class="container form_section-container">
         <h2>Change User Info</h2>
-        <form action="" enctype="multipart/form-data" >
+        <form action="<?=ROOT_URL?>admin/edit-user-logic.php" method="POST" >
             <div class="post__author">
                 <div class="post__author-avatar">
-                    <img src="/images/jisoo.jpg">
+                <img src="<?= ROOT_URL . 'images/' . $user['avatar'] ?>">
                 </div>
                 <div class="post__author">
-                    <h5> Username </h5>
+                    <h4> <?= $user['username']?> </h4>
                 </div>
             </div>
-            <input type="text" placeholder="First name">
-            <input type="text" placeholder="Last name">
-            <select >
+            <input type="text" name="firstname" value="<?= $user['firstname']?>" placeholder="First name">
+            <input type="text" name="lastname" value="<?= $user['lastname']?>" placeholder="Last name">
+            <select name="userrole" >
                 <option value="1">Author</option>
                 <option value="0">Moderator</option>
                 <option value="0">Admin</option>
             </select>
-            <button type="submit" class="btn">Update User Info</button>
+            <button type="submit" name="submit" class="btn">Update User Info</button>
          </form>
     </div>
 </section>
