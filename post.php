924879239c203125975b662c9f7a332227ae4c42
Joel Joseph <joeljosephpam@gmail.com>
2023-12-10 23:56:40 +0530

    post.php now dynamically load content based on the id passed

--- post.php
+++ post.php
@@ -1,31 +1,47 @@
 <?php 
     require 'partials/header.php';
+
+    if(isset($_GET['id'])){
+        $id = filter_var($_GET['id'],FILTER_SANITIZE_NUMBER_INT);
+        $query = "SELECT * FROM tb_posts WHERE id=$id";
+        $result = mysqli_query($connection, $query);
+        $post = mysqli_fetch_assoc($result);
+      } else {
+        header('location: '. ROOT_URL );
+      }
 ?>
 
 
     <section class="singlepost">
         <div class="container singlepost__container">
-            <h2> The assasination of Julius Ceaser</h2>
+            
+            <h2><?=$post['title']?></h2>
+            
             <div class="post__author">
+                <?php
+                    //fetch author info  [use forign key to improve]
+                    $author_id = $post['author_id'];
+                    $author_query = "SELECT * FROM tb_users WHERE id=$author_id";
+                    $author_result = mysqli_query($connection, $author_query);
+                    $author = mysqli_fetch_assoc($author_result);
+                    
+                ?>
+
                 <div class="post__author-avatar">
-                    <img src="/images/jisoo.jpg">
+                    <img src="images/<?=$author['avatar']?>">
                 </div>
+                
                 <div class="post__author-info.jpg">
-                    <h5> Steven Dicksirleot </h5>
-                    <small> Septemeber 11, 2001 </small>
-                </div>
-            </div>
-                <div class="singlepost__thumbnail">
-                    <img src="/images/b3.webp">
+                    <h5>By: <?="{$author['firstname']} {$author['lastname']}"?> </h5>
+                    <small> <?= date("d M, Y - H:i", strtotime($post['date_time']))?></small>
                 </div>
             
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius ipsa neque, repudiandae architecto, nostrum quam distinctio facilis fugit aliquam saepe pariatur laudantium libero ab. Ut aut dolorem facere at dolorum?</p>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius ipsa neque, repudiandae architecto, nostrum quam distinctio facilis fugit aliquam saepe pariatur laudantium libero ab. Ut aut dolorem facere at dolorum?</p>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius ipsa neque, repudiandae architecto, nostrum quam distinctio facilis fugit aliquam saepe pariatur laudantium libero ab. Ut aut dolorem facere at dolorum?</p>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius ipsa neque, repudiandae architecto, nostrum quam distinctio facilis fugit aliquam saepe pariatur laudantium libero ab. Ut aut dolorem facere at dolorum?</p>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius ipsa neque, repudiandae architecto, nostrum quam distinctio facilis fugit aliquam saepe pariatur laudantium libero ab. Ut aut dolorem facere at dolorum?</p>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius ipsa neque, repudiandae architecto, nostrum quam distinctio facilis fugit aliquam saepe pariatur laudantium libero ab. Ut aut dolorem facere at dolorum?</p>
-        </div>   
+            </div>
+            <div class="singlepost__thumbnail">
+                <img src="images/<?=$post['thumbnail']?>">
+                </div>
+                <p><?=$post['body']?></p>
+            </div>   
      </section>
 
 
