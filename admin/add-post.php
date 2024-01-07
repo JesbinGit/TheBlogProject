d5dfae2aa79996fb483d5a522cb954096bac0dd1
JesbinGit <jesbinytofficial@gmail.com>
2024-01-07 20:01:32 +0530

    add post fixes

--- admin/add-post.php
+++ admin/add-post.php
@@ -5,9 +5,9 @@
     $query = "SELECT * FROM tb_categories";
     $categories = mysqli_query($connection, $query);
 
-    //Recover entry if any error occured
- //   $title = $_SESSION['add-post-data']['title'] ?? null;
-    // $body = $_SESSION['add-post-data']['body'] ?? null;
+//Recover entry if any error occured
+   $title = $_SESSION['add-post-data']['title'] ?? null;
+     $body = $_SESSION['add-post-data']['body'] ?? null;
 
     unset($_SESSION['add-post-data']);
 ?>
@@ -30,9 +30,9 @@
         
         <form action="<?=ROOT_URL?>admin/add-post-logic.php" enctype="multipart/form-data" method="POST">
             
-            <input type="text" name="title" value="<?=$title?>" placeholder="Title">
+           <input type="text" name="title" value="<?=$title?>" placeholder="Title">
             
-            <select name="category" >
+            <select name="category" > 
                 
                 <?php while($category = mysqli_fetch_assoc($categories)) : ?>
                    
@@ -43,7 +43,7 @@
             </select>
 
             <textarea rows="10" name="body" placeholder="Body"><?=$body?></textarea>
-           
+                
             <?php if(isset($_SESSION['user_is_admin'])) : ?>
            
                 <div class="form_control inline">
